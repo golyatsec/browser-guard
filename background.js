d007458c01e6c35b9d67cd3a84509172e4f410de
Berkay Ahmet DAĞ <berkay.dag@golyatsec.com>
2024-04-03 23:55:05 +0300

    General Update v1.5
    USOM DB Added & Popup and Blocked Page Updated & Minimal Data Updated

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Phishing & Scam Manuel DB by Golyat Security
+//Created by Golyat Security
+//DB Update Date: 01.04.2024
+//For Your Cyber Security
+//All Rights Reserved
+//Golyatsec.com
+
 var domains;
 
 async function updateRules() {
@@ -44,8 +53,47 @@ function isPhishingDomain(domain) {
 }
 
 chrome.runtime.onMessage.addListener(function(message) {
-    if (message.type === "blocked_domain") {
+    if (message.type === "blocked-domain") {
        const blockedDomain = message.domain;
        chrome.storage.local.set({ "blockedDomain": blockedDomain });
    }
 });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Phishing & Scam Automatic DB by USOM
+//Created by Golyat Security
+//DB Update Rate: Every Hour
+//For Your Cyber Security
+//All Rights Reserved
+//Golyatsec.com
+
+async function checkPhishing(url) {
+    try {
+        const response = await fetch('https://www.usom.gov.tr/url-list.txt');
+        const text = await response.text();
+        const domains = text.split('\n');
+
+        const enteredDomain = new URL(url).hostname;
+
+        if (domains.includes(enteredDomain)) {
+            return true; 
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error("Error checking phishing:", error);
+        return false;
+    }
+}
+
+chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        const isPhishing = await checkPhishing(tab.url);
+        if (isPhishing) {
+            chrome.tabs.update(tabId, {url: chrome.runtime.getURL("/security/blocked.html")});
+        }
+    }
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
